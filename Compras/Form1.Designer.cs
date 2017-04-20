e888953531f1f4c8476ec706e12f2884447f9a33
esaimv <esaimv@gmail.com>
2017-04-20 16:18:11 -0700

    Hasta aqui parece que si jala

--- Compras/Form1.Designer.cs
+++ Compras/Form1.Designer.cs
@@ -253,11 +253,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-<<<<<<< HEAD
             this.label1.Location = new System.Drawing.Point(41, 36);
-=======
-            this.label1.Location = new System.Drawing.Point(50, 34);
->>>>>>> origin/master
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 13);
             this.label1.TabIndex = 4;
